5a476adc63df957baba6eb85b64177193117dd90
Afzal Khan <fzlkhan0@gmail.com>
2025-04-10 12:30:36 +1000

    Version 1.2 Fixed all issues execpt some ma=inor

--- portfolio/app.py
+++ portfolio/app.py
@@ -209,7 +209,7 @@ if __name__ == '__main__':
         os.makedirs(data_dir)
     
     # Get port from environment variable or use 5000 as default
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5001))
     
     # Run the app
     app.run(host='0.0.0.0', port=port, debug=False)
